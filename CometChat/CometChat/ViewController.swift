73394c3f07235530647ac16a9c2f9ce090a501e4
Angshuman <angshumandas2611@gmail.com>
2022-07-01 12:35:02 +0530

    Project setup

---
+++ CometChat/CometChat/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CometChat
+//
+//  Created by Angshuman Das on 10/04/1401 AP.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
